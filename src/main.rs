1a8d5a90f824948a9ef33e196548e657427532e0
Vladimir Komendantskiy <komendantsky@gmail.com>
2020-12-25 13:57:00 +0000

    added a few consts and a utility fn

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,24 @@ const PLATFORM_HFTHICKNESS: f32 = 0.2;
 const PLATFORM_HFSIDE: f32 = 2.0;
 const CYLINDER_HFHEIGHT: f32 = 0.6;
 const CYLINDER_RADIUS: f32 = 0.2;
+const STONEHENGE_RADIUS: f32 = 1.5;
+const NUM_PILLARS: usize = 12;
+const STONEHENGE_SECTOR: f32 = 2.0 * std::f32::consts::PI / (NUM_PILLARS as f32);
+
+fn centre_coords(i: usize) -> (f32, f32) {
+    let angle = i as f32 * STONEHENGE_SECTOR;
+    let x = angle.cos() * STONEHENGE_RADIUS;
+    let z = angle.sin() * STONEHENGE_RADIUS;
+    (x, z)
+}
 
 pub fn init_world(testbed: &mut Testbed) {
-    /*
-     * World
-     */
+    // World
     let mut bodies = RigidBodySet::new();
     let mut colliders = ColliderSet::new();
     let joints = JointSet::new();
 
-    /*
-     * Ground.
-     */
+    // Ground
     let ground_size = 10.0;
     let ground_height = 0.1;
 
@@ -33,21 +39,13 @@ pub fn init_world(testbed: &mut Testbed) {
     let collider = ColliderBuilder::cuboid(ground_size, ground_height, ground_size).build();
     colliders.insert(collider, handle, &mut bodies);
 
-    /*
-     * Create the pillars
-     */
-    let num_pillars: usize = 12;
-    let radius = 1.5;
+    // Create the pillars and bulkheads.
     let platform_top = PLATFORM_HEIGHT + PLATFORM_HFTHICKNESS;
     let pillar_y = platform_top + CYLINDER_HFHEIGHT;
     let bulkhead_y = platform_top + 2.0 * CYLINDER_HFHEIGHT + CYLINDER_RADIUS;
-    let sector = 2.0 * std::f32::consts::PI / num_pillars as f32;
 
-    for i in 0..num_pillars {
-        let angle = i as f32 * sector;
-        let x = angle.cos() * radius;
-        let z = angle.sin() * radius;
-
+    for i in 0..NUM_PILLARS {
+        let (x, z) = centre_coords(i);
         let pillar_body = RigidBodyBuilder::new_dynamic()
             .translation(x, pillar_y, z)
             .mass(1.0, true)
@@ -59,33 +57,30 @@ pub fn init_world(testbed: &mut Testbed) {
 
         if i % 2 == 1 {
             // TODO: avoid recomputing these values.
-            let angle0 = (i - 1) as f32 * sector;
-            let x0 = angle0.cos() * radius;
-            let z0 = angle0.sin() * radius;
-
+            let (x0, z0) = centre_coords(i - 1);
             let a = x0 - x;
             let b = z - z0;
             let bulkhead_angle = b.atan2(a);
-            println!("bulkhead angle {}", bulkhead_angle);
             let bulkhead_body = RigidBodyBuilder::new_dynamic()
                 .translation((x0 + x) / 2.0, bulkhead_y, (z0 + z) / 2.0)
                 .rotation(Vector3::y() * bulkhead_angle)
                 .mass(1.0, true)
                 .build();
             let bulkhead_handle = bodies.insert(bulkhead_body);
-            let bulkhead_length = sector;
-            let bulkhead_thickness = CYLINDER_RADIUS;
-            let bulkhead_width = CYLINDER_RADIUS * 1.1;
-            let bulkhead_collider =
-                ColliderBuilder::cuboid(bulkhead_length, bulkhead_thickness, bulkhead_width)
-                    .build();
+            let bulkhead_halflength = STONEHENGE_SECTOR;
+            let bulkhead_halfthickness = CYLINDER_RADIUS;
+            let bulkhead_halfwidth = CYLINDER_RADIUS * 1.1;
+            let bulkhead_collider = ColliderBuilder::cuboid(
+                bulkhead_halflength,
+                bulkhead_halfthickness,
+                bulkhead_halfwidth,
+            )
+            .build();
             colliders.insert(bulkhead_collider, bulkhead_handle, &mut bodies);
         }
     }
 
-    /*
-     * Setup a kinematic rigid body.
-     */
+    // Setup a kinematic rigid body.
     let platform_body = RigidBodyBuilder::new_kinematic()
         .translation(0.0, PLATFORM_HEIGHT, 0.0)
         .build();
@@ -94,9 +89,7 @@ pub fn init_world(testbed: &mut Testbed) {
         ColliderBuilder::cuboid(PLATFORM_HFSIDE, PLATFORM_HFTHICKNESS, PLATFORM_HFSIDE).build();
     colliders.insert(collider, platform_handle, &mut bodies);
 
-    /*
-     * Setup a callback to control the platform.
-     */
+    // Setup a callback to control the platform.
     let mut count = 0;
     testbed.add_callback(move |_, physics, _, _, time| {
         count += 1;
@@ -122,9 +115,7 @@ pub fn init_world(testbed: &mut Testbed) {
         }
     });
 
-    /*
-     * Run the simulation.
-     */
+    // Run the simulation.
     testbed.set_body_color(platform_handle, Point3::new(0.0, 1.0, 0.0));
     testbed.set_world(bodies, colliders, joints);
     testbed.look_at(Point3::new(-5.0, 10.0, -10.0), Point3::origin());
